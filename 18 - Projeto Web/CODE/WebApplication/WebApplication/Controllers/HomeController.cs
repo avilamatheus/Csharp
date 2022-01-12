9c4c615c30d1265269d1f555b99eab8ea6062b31
avilamatheus <matheus_avila17@hotmail.com>
2022-01-12 14:18:26 -0300

    MVC - Department

--- 18 - Projeto Web/CODE/WebApplication/WebApplication/Controllers/HomeController.cs
+++ 18 - Projeto Web/CODE/WebApplication/WebApplication/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication.Models;
+using WebApplication.Models.ViewModels;
 
 namespace WebApplication.Controllers
 {
---
+++ 18 - Projeto Web/CODE/WebApplication/WebApplication/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication.Models
+{
+    public class Department
+    {
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
