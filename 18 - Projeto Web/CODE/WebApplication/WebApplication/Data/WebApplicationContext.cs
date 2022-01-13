45f7afc3cbbca66193d1e483c5334d44357993ac
avilamatheus <matheus_avila17@hotmail.com>
2022-01-13 14:25:11 -0300

    outras entidades e 2a migracao

--- 18 - Projeto Web/CODE/WebApplication/WebApplication/Data/WebApplicationContext.cs
+++ 18 - Projeto Web/CODE/WebApplication/WebApplication/Data/WebApplicationContext.cs
@@ -13,6 +13,8 @@ namespace WebApplication.Models
         {
         }
 
-        public DbSet<WebApplication.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
